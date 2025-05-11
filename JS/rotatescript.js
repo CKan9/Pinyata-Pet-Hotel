4f2e4e901bab196777b771abd479af6a83718901
CKan9 <kangckchai@gmail.com>
2025-05-11 22:03:36 +0800

    Fix image paths in rotate script for correct loading

--- JS/rotatescript.js
+++ JS/rotatescript.js
@@ -1,6 +1,6 @@
-// script.js
+//  script.js
 document.addEventListener('DOMContentLoaded', () => {
-    const images = ['IMG/M1.webp', 'IMG/M2.webp', 'IMG/M3.webp', 'IMG/M4.webp'];
+    const images = ['/PT/IMG/M1.webp', '/PT/IMG/M2.webp', '/PT/IMG/M3.webp', '/PT/IMG/M4.webp'];
     let currentIndex = 0;
     const imgElement = document.getElementById('rotating-image');
 
