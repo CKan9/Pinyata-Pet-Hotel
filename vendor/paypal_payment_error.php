916ba9fa2e6202a3027ac854e85efc87aecf9821
CKan9 <kangckchai@gmail.com>
2025-06-04 22:32:40 +0800

    change many thing

--- vendor/paypal_payment_error.php
+++ vendor/paypal_payment_error.php
@@ -1,4 +1,4 @@
 <?php
 session_start();
-echo "<h2>Payment Cancelled</h2><p>Your payment was not completed. <a href='../HTML/booking.html'>Try Again</a></p>";
+echo "<h2>Payment Cancelled</h2><p>Your payment was not completed. <a href='../HTML/Booking.html'>Try Again</a></p>";
 ?>
